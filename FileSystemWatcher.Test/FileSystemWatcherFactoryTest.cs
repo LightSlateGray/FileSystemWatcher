587d8d9e9939e8b78e72ac36041571e9c5d78061
LightSlateGray <37671934+LightSlateGray@users.noreply.github.com>
2018-04-29 13:12:52 +0200

    fix(FileSystemWatcherFactoryTest.cs): update tests and add new tests
    - Update test cases to verify the expected result of the new boolean property 'Exists'
    - Update test cases to match the improved implementation
    - Add new test cases to verify that the expected exceptions will be thrown
    - Add test case to verify that an implementation of the IFileSystemWatcher interface will be created for a non-existent file within an existing directory structure

--- FileSystemWatcher.Test/FileSystemWatcherFactoryTest.cs
+++ FileSystemWatcher.Test/FileSystemWatcherFactoryTest.cs
@@ -87,6 +87,7 @@ namespace FileSytemWatcher.Test
 
             // Verify that the implementation of the IFileSystemWatcher interface returns the expected values
             Assert.AreEqual(FileSystemWatcherType.File, fileSystemWatcher.FileSystemWatcherType);
+            Assert.IsTrue(fileSystemWatcher.Exists, $"The file {fileInfo.FullName} should exist within the file system.");
             Assert.AreEqual(fileInfo.FullName, fileSystemWatcher.FullPath);
             Assert.AreEqual(fileInfo.Name, fileSystemWatcher.Name);
 
@@ -101,16 +102,17 @@ namespace FileSytemWatcher.Test
         [TestMethod]
         public void FileSystemWatcherInstanceForDirectoryCreationTest()
         {
-            // Retrieve information about the temporary file used within this test case
-            var fileInfo = new FileInfo(this.temporaryFileName);
+            // Retrieve information about the directory where the temporary file used within this test case resides in
+            var directoryInfo = new FileInfo(this.temporaryFileName).Directory;
             
             // Create a new implementation of the IFileSystemWatcher interface for a directory
-            var fileSystemWatcher = FileSystemWatcherFactory.CreateInstance(fileInfo.Directory.FullName);
+            var fileSystemWatcher = FileSystemWatcherFactory.CreateInstance(directoryInfo.FullName);
 
             // Verify that the implementation of the IFileSystemWatcher interface returns the expected values
             Assert.AreEqual(FileSystemWatcherType.Directory, fileSystemWatcher.FileSystemWatcherType);
-            Assert.AreEqual(fileInfo.Directory.FullName, fileSystemWatcher.FullPath);
-            Assert.AreEqual(fileInfo.Directory.Name, fileSystemWatcher.Name);
+            Assert.IsTrue(fileSystemWatcher.Exists, $"The directory {directoryInfo.FullName} should exist within the file system.");
+            Assert.AreEqual(directoryInfo.FullName, fileSystemWatcher.FullPath);
+            Assert.AreEqual(directoryInfo.Name, fileSystemWatcher.Name);
 
             // Verify that the implementation of the IFileSystemWatcher interface is not listening for changes by default
             Assert.IsFalse(fileSystemWatcher.IsWatching, $"A new FileSystemWatcher instance should not listen for changes by default.");
@@ -129,7 +131,7 @@ namespace FileSytemWatcher.Test
         [DataRow("This is not a valid path")]
         [DataRow("Ínv@l1d;Cháràçter?")]
         [DataRow("*.*")]
-        public void FileSystemWatcherThrowsExceptionForInvalidFile(string path)
+        public void FileSystemWatcherThrowsExceptionForInvalidPath(string path)
         {
             // Passing invalid values as arguments to the factory will throw an exception
             var argumentException = Assert.ThrowsException<ArgumentException>(
@@ -139,6 +141,92 @@ namespace FileSytemWatcher.Test
                 "path",
                 argumentException.ParamName,
                 $"The {nameof(ArgumentException)} should blame parameter 'path' as cause.");
+        }
+
+        /// <summary>
+        ///   Verifies that calling <see cref="FileSystemWatcherFactory.CreateInstance(string, IFileSystemWatcherEventHandler)"/>
+        ///   to watch for a non-existent directory will throw an exception, because the <see cref="FileSystemWatcher"/> instance used
+        ///   internally does not support watching non-existent directories.
+        /// </summary>
+        [TestMethod]
+        public void FileSystemWatcherThrowsExceptionForNonExistentPath()
+        {
+            // Search for a non-existent temporary directory
+            var tempDirectory = Path.GetTempPath();
+            var directoryName = Path.GetRandomFileName();
+            while (Directory.Exists(Path.Combine(tempDirectory, directoryName)))
+            {
+                directoryName = Path.GetRandomFileName();
+            }
+
+            // Retrieve information about the temporary directory used within this test case
+            var directoryInfo = new DirectoryInfo(Path.Combine(tempDirectory, directoryName));
+            var directoryPath = directoryInfo.FullName + Path.DirectorySeparatorChar;
+
+            // Passing a non-existent path as argument to the factory will throw an exception
+            var argumentException = Assert.ThrowsException<ArgumentException>(
+                () => FileSystemWatcherFactory.CreateInstance(directoryPath),
+                $"Calling {nameof(FileSystemWatcherFactory.CreateInstance)} while passing the invalid value '{directoryPath}' as argument should throw an exception.");
+            Assert.AreEqual(
+                "path",
+                argumentException.ParamName,
+                $"The {nameof(ArgumentException)} should blame parameter 'path' as cause.");
+        }
+
+        /// <summary>
+        ///   Verifies that calling <see cref="FileSystemWatcherFactory.CreateInstance(string, IFileSystemWatcherEventHandler)"/>
+        ///   to watch for a file within a non-existent directory will throw an exception, because the <see cref="FileSystemWatcher"/>
+        ///   instance used internally does not support watching non-existent directories.
+        /// </summary>
+        [TestMethod]
+        public void FileSystemWatcherThrowsExceptionForFileWithinNonExistentPath()
+        {
+            // Search for a non-existent temporary directory
+            var tempDirectory = Path.GetTempPath();
+            var directoryName = Path.GetRandomFileName();
+            while (Directory.Exists(Path.Combine(tempDirectory, directoryName)))
+            {
+                directoryName = Path.GetRandomFileName();
+            }
+
+            // Retrieve information about the temporary directory used within this test case
+            var directoryInfo = new DirectoryInfo(Path.Combine(tempDirectory, directoryName));
+            var fileWithinNonExistantDirectoryPath = Path.Combine(directoryInfo.FullName, Path.GetRandomFileName());
+
+            // Passing a file within a non-existent path as argument to the factory will throw an exception
+            var argumentException = Assert.ThrowsException<ArgumentException>(
+                () => FileSystemWatcherFactory.CreateInstance(fileWithinNonExistantDirectoryPath),
+                $"Calling {nameof(FileSystemWatcherFactory.CreateInstance)} while passing the invalid value '{fileWithinNonExistantDirectoryPath}' as argument should throw an exception.");
+            Assert.AreEqual(
+                "path",
+                argumentException.ParamName,
+                $"The {nameof(ArgumentException)} should blame parameter 'path' as cause.");
+        }
+
+        /// <summary>
+        ///   Verifies that calling <see cref="FileSystemWatcherFactory.CreateInstance(string, IFileSystemWatcherEventHandler)"/>
+        ///   supplying a path to a non-existent file will create a valid implementation of the <see cref="IFileSystemWatcher"/> interface.
+        /// </summary>
+        [TestMethod]
+        public void FileSystemWatcherInstanceForNonExistentFileCreationTest()
+        {
+            // Retrieve information about the temporary file used within this test case
+            var fileInfo = new FileInfo(this.temporaryFileName);
+
+            // Delete the file
+            fileInfo.Delete();
+
+            // Create a new implementation of the IFileSystemWatcher interface for a non-existent file
+            var fileSystemWatcher = FileSystemWatcherFactory.CreateInstance(fileInfo.FullName);
+
+            // Verify that the implementation of the IFileSystemWatcher interface returns the expected values
+            Assert.AreEqual(FileSystemWatcherType.File, fileSystemWatcher.FileSystemWatcherType);
+            Assert.IsFalse(fileSystemWatcher.Exists, $"The file {fileInfo.FullName} should not exist within the file system.");
+            Assert.AreEqual(fileInfo.FullName, fileSystemWatcher.FullPath);
+            Assert.AreEqual(fileInfo.Name, fileSystemWatcher.Name);
+
+            // Verify that the implementation of the IFileSystemWatcher interface is not listening for changes by default
+            Assert.IsFalse(fileSystemWatcher.IsWatching, $"A new FileSystemWatcher instance should not listen for changes by default.");
         }
     }
 }
